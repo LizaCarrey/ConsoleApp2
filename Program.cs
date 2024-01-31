5bc719c9f2569c669292f4f995d7334fce4e3fb3
Kab-31-13 <Kab-31-13@KAB-31-13-PC>
2024-01-31 09:39:03 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Вывести в строку десять заглавных букв "а" через один пробел:
+
+            //for (int x = 1;x<= 10;x++)
+            //{
+            //    Console.Write("A ");
+            //}
+
+
+            //2.Вывести в обратном порядке числа от 50 до 40.
+
+            //for (int x = 50; x >= 40; x--)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+
+            //3.Вывести в строку через пробел следующие натуральные числа:
+
+            //6    12   18   24      30      36
+
+            //for (int x = 6; x <= 36; x+=6)
+            //{
+            //    Console.Write($"{x} ");
+            //}
+
+
+            //4. Вывести двузначные числа, кратные 11, подсчитать их количество и сумму.
+            //for (int x = 11; x <= 99; x++)
+            //    if (x%11 == 0) 
+            //{
+            //    Console.Write($"{x} ");
+            //}
+            //    else
+            //    {
+            //        Console.Write("");
+            //    }
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
